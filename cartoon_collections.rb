ba6b987423ec6ce2c922faa9bc57fcc9a47d4d6c
Zachary Durr <zdurr22@gmail.com>
2020-10-05 16:08:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  veggies.map do |call|
 end
 
 def long_planeteer_calls# code an argument here
