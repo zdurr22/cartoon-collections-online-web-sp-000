f1bd87b849b552e2056cc27590fdec3019ff0681
Zachary Durr <zdurr22@gmail.com>
2020-10-05 15:57:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each do |dwarf|
+    dwarf
+  end
 end
 
 def summon_captain_planet# code an argument here
