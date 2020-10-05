17c19daf1a3582648f187b9fb32f2a52b5db9d62
Zachary Durr <zdurr22@gmail.com>
2020-10-05 16:33:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(call)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_type)# code an argument here
   # the array below is here to help
+  cheese_type.find do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
